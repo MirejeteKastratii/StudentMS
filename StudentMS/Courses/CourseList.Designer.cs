e4c20f06da7587a3c671ebfa8dd40a6cb096b554
Florentinehamiti <85936110+Florentinehamiti@users.noreply.github.com>
2021-11-15 10:02:03 -1100

    Course Forms added

--- StudentMS/Courses/CourseList.Designer.cs
+++ StudentMS/Courses/CourseList.Designer.cs
@@ -29,28 +29,81 @@ namespace StudentMS.Courses
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.dgvListaKurseve = new System.Windows.Forms.DataGridView();
+            this.lblListaKurseve = new System.Windows.Forms.Label();
+            this.btnRifreskoKurset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaKurseve)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // dgvListaKurseve
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(267, 96);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Course :ist";
+            this.dgvListaKurseve.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaKurseve.Location = new System.Drawing.Point(14, 103);
+            this.dgvListaKurseve.Name = "dgvListaKurseve";
+            this.dgvListaKurseve.RowHeadersWidth = 51;
+            this.dgvListaKurseve.RowTemplate.Height = 24;
+            this.dgvListaKurseve.Size = new System.Drawing.Size(700, 404);
+            this.dgvListaKurseve.TabIndex = 0;
+            // 
+            // lblListaKurseve
+            // 
+            this.lblListaKurseve.AutoSize = true;
+            this.lblListaKurseve.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblListaKurseve.Location = new System.Drawing.Point(38, 48);
+            this.lblListaKurseve.Name = "lblListaKurseve";
+            this.lblListaKurseve.Size = new System.Drawing.Size(170, 26);
+            this.lblListaKurseve.TabIndex = 1;
+            this.lblListaKurseve.Text = "Lista e kurseve :";
+            // 
+            // btnRifreskoKurset
+            // 
+            this.btnRifreskoKurset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRifreskoKurset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F);
+            this.btnRifreskoKurset.Location = new System.Drawing.Point(303, 40);
+            this.btnRifreskoKurset.Name = "btnRifreskoKurset";
+            this.btnRifreskoKurset.Size = new System.Drawing.Size(133, 47);
+            this.btnRifreskoKurset.TabIndex = 2;
+            this.btnRifreskoKurset.Text = "Rifresko";
+            this.btnRifreskoKurset.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(442, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Edito rekordin";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(581, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Fshij rekordin";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // CourseList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(726, 519);
-            this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnRifreskoKurset);
+            this.Controls.Add(this.lblListaKurseve);
+            this.Controls.Add(this.dgvListaKurseve);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CourseList";
             this.Text = "CourseList";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaKurseve)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +111,10 @@ namespace StudentMS.Courses
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvListaKurseve;
+        private System.Windows.Forms.Label lblListaKurseve;
+        private System.Windows.Forms.Button btnRifreskoKurset;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
