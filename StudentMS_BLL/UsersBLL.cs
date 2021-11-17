9b3c737835ca38a5db6c10ccc3ff81b052cefdf8
Florentinehamiti <85936110+Florentinehamiti@users.noreply.github.com>
2021-11-17 02:18:58 -1100

    Select and delete users

---
+++ StudentMS_BLL/UsersBLL.cs
@@ -0,0 +1,28 @@
+﻿using StudentMS_DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMS_BLL
+{
+    public class UsersBLL
+    {
+        private readonly UsersDAL usersDAL;
+        public UsersBLL()
+        {
+            usersDAL = new UsersDAL();
+        }
+
+        public DataTable ShowUsers()
+        {
+            return usersDAL.GetUsersList();
+        }
+        public bool DeleteUser(int ID)
+        {
+            return usersDAL.DeleteUsers(ID);
+        }
+    }
+}
---
+++ StudentMS_DAL/UsersDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMS_DAL
+{
+    public class UsersDAL
+    {
+        public DataTable GetUsersList()
+        {
+            using (DbConn.conn = new SqlConnection(DbConn.connString))
+            {
+                DbConn.dataAdapter = new SqlDataAdapter("usp_UsersList", DbConn.conn);
+                DataTable dt = new DataTable();
+                DbConn.dataAdapter.Fill(dt);
+                return dt;
+            }
+        }
+        public bool DeleteUsers(int ID)
+        {
+            try
+            {
+                using (DbConn.conn = new SqlConnection(DbConn.connString))
+                {
+                    DbConn.conn.Open();
+                    DbConn.cmd = new SqlCommand("usp_DeleteUsers", DbConn.conn);
+                    DbConn.cmd.CommandType = CommandType.StoredProcedure;
+
+                    DbConn.cmd.Parameters.AddWithValue("@ID", ID);
+
+                    DbConn.cmd.ExecuteNonQuery();
+                    DbConn.conn.Close();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
